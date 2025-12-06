4a209ab2977e36833221f29965d06e446d55961a
curtasdeveloper <louisecurtandrei.pacete@gmail.com>
2025-12-06 11:59:48 +0800

    Connected the server to database

--- backend/server.js
+++ backend/server.js
@@ -8,16 +8,32 @@ import helloWorldRouter from './src/routes/helloWorldRoute.js';
 const app = express();
 const port = process.env.PORT || 3000;
 const hostname = process.env.HOSTNAME || "localhost";
+const databaseURI = process.env.DATABASE_URI;
 
 // MIDDLEWARE
 app.use(cors(corsConfig))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
 
+const connectDB = async () => {
+    try {
+        const connect = await mongoose.connect(databaseURI)
+        if (connect) {
+            console.log(`✓ Successfully connected to MongoDB`);
+        }
+    } catch (error) {
+        console.error('✗ MongoDB connection error:', error.message);
+    }
+}
+
 // ROUTES
 app.use('/api', helloWorldRouter)
 
+const startServer = async () => {
+    await connectDB()
+    app.listen(port, hostname, () => {
+        console.log(`✓ Server running on http://${hostname}:${port}`);
+    })
+}
 
-app.listen(port, hostname, () => {
-    console.log(`✓ Server running on http://${hostname}:${port}`);
-})
+startServer()
